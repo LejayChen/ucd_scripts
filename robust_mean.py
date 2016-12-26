fc71ed131ac99cc04614531f92f8b1f269a391e6
LejayChen <fiorano_chen@sjtu.edu.cn>
2016-12-26 17:13:29 +0800

    add intro to the function

--- robust_mean.py
+++ robust_mean.py
@@ -1,10 +1,19 @@
 import  numpy as np 
 
-'''
-  INPUT: 
-      x: 1D numpy array
-'''
 def robust_mean(x,iter=1,show_step=False):
+	'''
+	Robust Statistics Function, cast away data point out of 3 sigmas each iteration
+	Based on numpy
+
+	INPUT: 
+		x: 1D numpy array
+		   the array to do statistics on
+		iter: int 
+		    number of iterations (iter=0 means normal np.mean())
+		show_step: boolean
+		    if Ture, will print mean and std of each step
+
+	'''
 	for i in range(iter):
 		mean = np.mean(x)
 		std = np.std(x)
