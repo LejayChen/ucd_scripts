f449eb6b2d0ab2bffa4e05853ae390029fbdabe0
LejayChen <fiorano_chen@sjtu.edu.cn>
2017-02-22 22:59:09 +0800

    add color tags

--- clustering.py
+++ clustering.py
@@ -1,9 +1,10 @@
 from astropy.table import *
 from math import *
 from robust_mean import *
+from func import func
 import numpy as np
 import matplotlib.pyplot as plt
-DIS = 22.2*1000 #kpc
+DIS = 17.21*1000 #kpc
 M87 = (187.70583, 12.39111)
 
 cat_ucd = Table.read('NGVS.pilot.92ucds.fits')  #catalog of UCDs
@@ -15,9 +16,12 @@ dec_gc = np.array(cat_gc['dec'])  #Dec of GCs
 step = 0.5  # in kpc
 radius = np.arange(0.25,4.25,step)   # in kpc  from 0.25 to 3.75 step 0.5
 str_radius = str(radius).strip('[]').split()  #for table name
-
-tab_names = ['ID','ra','dec','DIS','r_h']+ str_radius 
-tab_dtype = ['a8','f8','f8','f4','f4']+['f4']*len(radius)
+tab_names = ['ID','ra','dec','DIS','r_h','g-i']
+for str_r in str_radius:
+	tab_names.append(str_r+'_count')
+	tab_names.append(str_r+'_count_cor')
+	tab_names.append(str_r+'_C')
+tab_dtype = ['a8','f8','f8','f4','f4','f4']+['i2','f2','f4']*len(radius)
 ucd_data = Table(names=(tab_names),dtype=(tab_dtype))
 
 gc_count_total = 0
@@ -30,19 +34,23 @@ for i in range(len(cat_ucd)):
 	ra_ucd = cat_ucd[i]['RA']
 	dec_ucd = cat_ucd[i]['DEC']
 	r_h = cat_ucd[i]['RH']
+	g_i = cat_ucd[i]['MAG_BEST'][1] - cat_ucd[i]['MAG_BEST'][3] 
 
 	dis_M87_ucd = sqrt((ra_ucd-M87[0])**2+(dec_ucd-M87[1])**2)/180.*pi*DIS  # in kpc (scalar)
 	dis_ucd_gc = np.sqrt((ra_gc-ra_ucd)**2+(dec_gc-dec_ucd)**2)/180.*pi*DIS  # in kpc (list)
-	if dis_M87_ucd>250. or dis_M87_ucd<20.:
+	if dis_M87_ucd>190 or dis_M87_ucd<15:
 		continue
-	exp_density = exp(5.3328)*dis_M87_ucd**(-1.9161)   # expected density (assume uniform in the vicinity of a UCD)
-	bkg_unif =  0   # uniform bakground in the field (possibly non-GC objects)
-	col_value = [ID,ra_ucd,dec_ucd,round(dis_M87_ucd,2),r_h]
+	
+	exp_density = exp(5.10118386605)*dis_M87_ucd**(-1.85088530618 )   # expected density (assume uniform in the vicinity of a UCD)
+	bkg_unif = exp(5.10118386605)*200**(-1.85088530618)    # uniform bakground in the field (possibly non-GC objects)	
+	#exp_density = func(((ra_ucd-M87[0])/180*pi*DIS,(dec_ucd-M87[1])/180*pi*DIS), -1.732794,  123.30887701)  
+	#bkg_unif = 0                                                                        
+	col_value = [ID,ra_ucd,dec_ucd,round(dis_M87_ucd,2),r_h, g_i]
 
 	dis_ucd_gc_mask = np.array([])
 	print '============',str(ID),ra_ucd,dec_ucd,round(dis_M87_ucd,4),'================='
 	for r in radius:
-                          #mask out the area for counting  (ring shaped mask from r to r+step)
+                            #mask out the area for counting  (ring shaped mask from r to r+step)
 		dis_ucd_gc_mask = dis_ucd_gc[dis_ucd_gc<(r+step)] #outer bound
 		dis_ucd_gc_mask = dis_ucd_gc_mask[dis_ucd_gc_mask>r]  #inner bound
 		area = pi*((r+step)**2-r**2)  # area of that ring  (in kpc^2)
@@ -55,7 +63,9 @@ for i in range(len(cat_ucd)):
 		#Clustering Signal
 		C = np.append(C,gc_count_cor/gc_expected) 
 		gc_counts = np.append(gc_counts,gc_count)
-		col_value.append(len(dis_ucd_gc_mask))
+		col_value.append(gc_count)
+		col_value.append(gc_count_cor)
+		col_value.append(gc_count_cor/gc_expected)
 
 		# gc distance to host UCD
 		if gc_count == 0:
@@ -76,10 +86,10 @@ for i in range(len(cat_ucd)):
 dis_ucd_gc_list = dis_ucd_gc_list.reshape(len(dis_ucd_gc_list)/len(radius),len(radius)) 
 C = C.reshape(len(C)/len(radius),len(radius)) 
 gc_counts = gc_counts.reshape(len(gc_counts)/len(radius),len(radius)) 
-C = C[C[:,0]+C[:,1]>0]
+#C = C[C[:,0]+C[:,1]>0]
 C_mean = np.array([])
 C_std = np.array([])
-for i in range(len(radius)):# histagram for C signal distribution 
+for i in range(len(radius)):# histogram for C signal distribution 
 	print '========= C statistics for r =',radius[i],'============'
 	C_r = C[:,i]
 	C_r,mean_r,std_r = robust_mean(C[:,i],iter=0,show_step=True)
@@ -92,12 +102,12 @@ for i in range(len(radius)):# histagram for C signal distribution
 	fig2, ax2 = plt.subplots()
 	ax2.hist(gc_counts[:,i],bins=range(0, 8, 1))
 	ax2.set_title(str(radius[i])+'~'+str(radius[i]+step))
-	plt.savefig('C.gc.count.hist.'+str(radius[i])+'.png')
+	plt.savefig('gc.count.hist.'+str(radius[i])+'.png')
 
 	C_mean = np.append(C_mean,mean_r)   #robust averge C signal over all UCDs
-	C_std = np.append(C_std,std_r)   #robust averge C signal over all UCDs
+	C_std = np.append(C_std,std_r)  #robust averge C signal over all UCDs
 C_max = np.max(C,0)  #max
-C_min = np.min(C,0)  #min
+C_min = np.min(C,0)    #min
 dis_ucd_gc_list_mean = np.nanmean(dis_ucd_gc_list,0)
 
 print '=================C clustering signal======================'
@@ -106,7 +116,7 @@ print 'Number of UCDs:',C.shape[0],', Number of GCs:',gc_count_total
 print 'Radius bins:',radius
 print 'Mean DIS:',dis_ucd_gc_list_mean
 print 'Mean:',C_mean
-print 'Std:',C_std
+print 'Std:',C_std/sqrt(len(cat_ucd)) 
 print 'Max:',C_max
 print 'Min:', C_min
 ucd_data.write('ucd_gc_clustering.fits',overwrite=True)
@@ -115,10 +125,11 @@ ucd_data.write('ucd_gc_clustering.fits',overwrite=True)
 fig, ax = plt.subplots()
 ax.errorbar(dis_ucd_gc_list_mean,C_mean,yerr=C_std/sqrt(len(cat_ucd)),fmt='o')
 ax.axhline(y=1,xmin=0,xmax=4,color='k')
-ax.set_xlabel('Aperture around UCD [kpc]')
-ax.set_ylabel(r'C=<$\Sigma_{measured}/\Sigma_{expected}$>',fontsize=16)
+ax.set_xlabel('Aperture around UCD [kpc]',fontsize=16)
+ax.set_ylabel(r'C=<$\Sigma_{\rm measured}/\Sigma_{\rm expected}$>',fontsize=17)
 ax.tick_params(which='major',length=12) 
 ax.tick_params(which='minor',length=5) 
 ax.set_xlim(0,4.25)
 ax.set_ylim(0,int(C_mean.max())+2)
 plt.savefig('clustering.new.png')
+plt.savefig('./pics/clustering_'+str_radius[0]+'_'+str(step)+'.eps')
--- tag_gc_to_ucd.py
+++ tag_gc_to_ucd.py
@@ -1,12 +1,20 @@
 import aplpy 
 from astropy.table import Table
 from astropy.io import fits
-from math import pi
+from math import pi,sqrt
+
 cat_ucd = Table.read('NGVS.pilot.92ucds.fits')  #catalog of UCDs
+
 cat_gc = Table.read('ngvs_pilot_xdclass1.0_g18.0-25.0.fits')
 cat_gc = cat_gc[cat_gc['p_gc']>0.95]   #masked (p_gc>0.95) catalog of GCs
-aperture =12./3600   # in degree
-DM = 22.2*1000 #kpc
+
+cat_v_ucd = Table.read('match_ucd_v.fits')
+cat_v_gc = Table.read('match_v_radial_gc.fits')
+cat_v_gc = cat_v_gc[cat_v_gc['p_gc']>0.95]
+
+aperture = 50./3600   # in degree
+DIS = 17.21*1000 #kpc
+M87 = (187.70583,  12.39111)
 
 def pixel_max(filename):
 	im = fits.open('ucd.'+filename+'.fits')
@@ -16,28 +24,55 @@ def pixel_max(filename):
 	pixel_value_central = data[x/2-2:x/2+2,y/2-3:y/2+2].max()
 	return pixel_value_central*1.1
 
-for i in range(len(cat_ucd)):
-	INDEX = cat_ucd[i]['INDEX']
-	ra_ucd = cat_ucd[i]['RA']
-	dec_ucd = cat_ucd[i]['DEC']
-
-	mask1 =  abs(cat_gc['ra'] -ra_ucd)<aperture
-	cat_gc_around_ucd = cat_gc[mask1]
+def mask(cat):
+	mask1 =  abs(cat['ra'] -ra_ucd)<aperture
+	cat_gc_around_ucd = cat[mask1]
 	mask2 =  abs(cat_gc_around_ucd['dec'] -dec_ucd)<aperture 
 	cat_gc_around_ucd = cat_gc_around_ucd[mask2]
+	return cat_gc_around_ucd
+
+for i in range(len(cat_v_ucd)):
+	INDEX = cat_v_ucd[i]['INDEX']
+	ra_ucd = cat_v_ucd[i]['RA']
+	dec_ucd = cat_v_ucd[i]['DEC']
+	dis = round(sqrt((ra_ucd-M87[0])**2+(dec_ucd-M87[1])**2)*pi/180*DIS,1)
+	v = round(cat_v_ucd[i]['v_msc'],2)
+	g_i_ucd = cat_v_ucd[i]['MAG_BEST'][1] - cat_ucd[i]['MAG_BEST'][3]  
+
+             #mask the GC catalog around UCD
+	cat_gc_around_ucd = mask(cat_gc)
 	ra_gc = cat_gc_around_ucd['ra']
 	dec_gc = cat_gc_around_ucd['dec']
+	g_i_gc = cat_gc_around_ucd['gmag'] - cat_gc_around_ucd['imag']
 
+             #mask the GC with radial velocity catalog around UCD
+	cat_gc_ucd_v = mask(cat_v_gc)
+	ra_gc_v = cat_gc_ucd_v['ra']
+	dec_gc_v = cat_gc_ucd_v['dec']
+	gc_v = cat_gc_ucd_v['v_msc']
 
 	filename = str(INDEX)
 	fig = aplpy.FITSFigure('ucd_newcut.'+filename+'.fits')
+	fig.tick_labels.set_xformat('dd.ddd')
+	fig.tick_labels.set_yformat('dd.ddd')
+
+	for i in range(len(ra_gc_v)):
+		fig.add_label(ra_gc_v[i],dec_gc_v[i] + 0.0004,text=str(gc_v[i]),color='red')
+	for i in range(len(ra_gc)):
+		fig.add_label(ra_gc[i],dec_gc[i] - 0.0004,text=str(g_i_gc[i]),color='green')
+	fig.add_label(ra_ucd,dec_ucd+0.0004,text=str(v))
+	fig.add_label(ra_ucd,dec_ucd-0.0004,text=str(g_i_ucd),color='green')
+
 	fig.show_grayscale(stretch='log',vmin=0,vmid=-0.3,vmax=pixel_max(filename),pmin=30,invert='y')
-	fig.show_circles(ra_ucd,dec_ucd,0.25/DM/pi*180,color='green')
-	fig.show_circles(ra_ucd,dec_ucd,0.75/DM/pi*180,color='green')
-	fig.show_circles(ra_ucd,dec_ucd,1.25/DM/pi*180,color='green')
-	fig.show_circles(ra_ucd,dec_ucd,1.75/DM/pi*180,color='green')
-	fig.show_circles(ra_ucd,dec_ucd,2.25/DM/pi*180,color='green')
-	fig.show_circles(ra_ucd,dec_ucd,2.75/DM/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,0.25/DIS/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,0.75/DIS/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,1.25/DIS/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,1.75/DIS/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,2.25/DIS/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,2.75/DIS/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,3.25/DIS/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,3.75/DIS/pi*180,color='green')
+	fig.show_circles(ra_ucd,dec_ucd,4.25/DIS/pi*180,color='green')
 	if len(cat_gc_around_ucd)>0: fig.show_circles(ra_gc,dec_gc,0.8/3600,color='red')
-	fig.set_title('UCD '+filename)
+	fig.set_title('UCD '+filename+'  '+str(dis))
 	fig.save('ucd_gctag.'+filename+'.png')
