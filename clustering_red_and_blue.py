9ff9a1e8dc5ac536b9097f813a5109f96506207c
LejayChen <fiorano_chen@sjtu.edu.cn>
2017-03-05 16:32:38 +0800

    fixed some bugs

--- clustering_red_and_blue.py
+++ clustering_red_and_blue.py
@@ -27,8 +27,11 @@ cat_gc_r = cat_gc[cat_gc['gmag'] - cat_gc['imag']>0.89]
 ra_gc_r = np.array(cat_gc_r['ra'])      #RA of GCs
 dec_gc_r = np.array(cat_gc_r['dec'])  #Dec of GCs
 
+fit_min = 15
+fit_max = 230
 step = 0.5  # in kpc
-radius = np.arange(0.25,4.25,step)   # in kpc  from 0.25 to 3.75 step 0.5
+start = 0.5
+radius = np.arange(start, start+4, step)   # in kpc  from 0.25 to 3.75 step 0.5
 str_radius = str(radius).strip('[]').split()  #for table name
 tab_names = ['ID','ra','dec','DIS','r_h','g-i']
 for str_r in str_radius:
@@ -73,7 +76,7 @@ def bkg_2d(ra,dec):
 
 def bkg_1d(dis_M87_ucd,slope, intercept):
 	exp_density = exp(intercept)*dis_M87_ucd**(slope)  # expected density (assume uniform in the vicinity of a UCD)
-	bkg_unif = 0#exp(intercept)*270**(slope)    # uniform bakground in the field (possibly non-GC objects)	
+	bkg_unif = exp(intercept)*270**(slope)    # uniform bakground in the field (possibly non-GC objects)	
 	return exp_density,bkg_unif
 
 def gc_stat(dis_ucd_gc, exp_density, bkg_unif):
@@ -94,7 +97,7 @@ def gc_stat(dis_ucd_gc, exp_density, bkg_unif):
 
 	return gc_count, gc_count_cor, gc_expected, dis_ucd_gc_mask
 
-def C_ave(gc_counts, dis_ucd_gc_list, C):
+def C_ave(gc_counts, dis_ucd_gc_list, C, mask):
 	'''
 	calculate averaged C value among UCDs
 	dis_ucd_gc_list_mean: distance from GC to UCD in each bin averaged for all UCDs (list)
@@ -102,18 +105,17 @@ def C_ave(gc_counts, dis_ucd_gc_list, C):
 	dis_ucd_gc_list = dis_ucd_gc_list.reshape(len(dis_ucd_gc_list)/len(radius),len(radius)) 
 	dis_ucd_gc_list_mean = np.nanmean(dis_ucd_gc_list,0) 
 
+	C = C.reshape(len(C)/len(radius),len(radius))
 	gc_counts = gc_counts.reshape(len(gc_counts)/len(radius),len(radius)) 
 	gc_counts_mean = np.nanmean(gc_counts,0) 
 
-	C = C.reshape(len(C)/len(radius),len(radius)) 
-
-	#C = C[gc_counts[:,0]+gc_counts[:,1]>0]
+	#C = C[mask]
 	C_mean = np.array([])
 	C_std = np.array([])
 	for i in range(len(radius)):
 		logfile.write('========= C statistics for r ='+str(radius[i])+'============\n')
 		C_cloumn = C[:,i]
-		C_cloumn, mean, std,robust_mean_log = robust_mean(C[:,i],iter=0,show_step=True)
+		C_cloumn, mean, std,robust_mean_log = robust_mean(C[:,i],iter=1,show_step=True)
 		logfile.write(robust_mean_log)
 		C_mean = np.append(C_mean,mean)   #robust averge C signal over all UCDs
 		C_std = np.append(C_std,std)  #robust averge C signal over all UCDs
@@ -141,7 +143,8 @@ for i in range(len(cat_ucd)):
 	g_i = cat_ucd[i]['MAGCOR_AP8'][1] - cat_ucd[i]['MAGCOR_AP8'][3] 
 
 	dis_M87_ucd = sqrt((ra_ucd-M87[0])**2+(dec_ucd-M87[1])**2)/180.*pi*DIS
-	if dis_M87_ucd>190 or dis_M87_ucd<15:
+
+	if dis_M87_ucd>fit_max or dis_M87_ucd<fit_min:
 		continue
 
 	exp_density,bkg_unif = bkg_1d(dis_M87_ucd,slope = -1.89827756001, intercept = 5.26216641247)
@@ -194,37 +197,43 @@ for i in range(len(cat_ucd)):
 	ucd_data.add_row(col_value)
 
 logfile.write('=================C clustering signal======================\n')
-C_mean, C_std, dis_ucd_gc_list_mean, gc_counts_mean = C_ave(gc_counts, dis_ucd_gc_list, C)
-C_mean_b, C_std_b, dis_ucd_gc_list_mean_b, gc_counts_mean_b = C_ave(gc_counts_b, dis_ucd_gc_list_b, C_b)
-C_mean_r, C_std_r, dis_ucd_gc_list_mean_r, gc_counts_mean_r = C_ave(gc_counts_r, dis_ucd_gc_list_r, C_r)
+
+gc_counts = gc_counts.reshape(len(gc_counts)/len(radius),len(radius)) 
+mask = gc_counts[:,0] +gc_counts[:,1] >0
+gc_counts = gc_counts.ravel()
+
+C_mean, C_std, dis_ucd_gc_list_mean, gc_counts_mean = C_ave(gc_counts, dis_ucd_gc_list, C, mask)
+C_mean_b, C_std_b, dis_ucd_gc_list_mean_b, gc_counts_mean_b = C_ave(gc_counts_b, dis_ucd_gc_list_b, C_b, mask)
+C_mean_r, C_std_r, dis_ucd_gc_list_mean_r, gc_counts_mean_r = C_ave(gc_counts_r, dis_ucd_gc_list_r, C_r, mask)
 
 np.set_printoptions(precision=2)
-print 'Number of UCDs:',C.shape[0]/len(radius)
+no_ucd = C.shape[0]/len(radius)
+print 'Number of UCDs:', no_ucd
 print 'Radius bins:',radius
 print '==============================='
 print 'Mean DIS:',dis_ucd_gc_list_mean
 print 'Mean C:',C_mean
 print 'GC counts Mean:',gc_counts_mean
-print 'Std:',C_std/sqrt(len(cat_ucd)) 
+print 'Std:',C_std/sqrt(no_ucd) 
 print '==============================='
 print 'Blue Mean DIS:',dis_ucd_gc_list_mean_b
 print 'Blue Mean C:',C_mean_b
 print 'Blue GC counts Mean:',gc_counts_mean_b
-print 'Blue Std:',C_std_b/sqrt(len(cat_ucd)) 
+print 'Blue Std:',C_std_b/sqrt(no_ucd) 
 print '==============================='
 print 'Red Mean DIS:',dis_ucd_gc_list_mean_r
 print 'Red Mean C:',C_mean_r
 print 'Red GC counts Mean:',gc_counts_mean_r
-print 'Red Std:',C_std_r/sqrt(len(cat_ucd)) 
+print 'Red Std:',C_std_r/sqrt(no_ucd) 
 #print 'Max:',C_max
 #print 'Min:', C_min
 ucd_data.write('clustering.fits',overwrite=True)
 
 #plot the final figure
 fig, ax = plt.subplots()
-ax.errorbar(dis_ucd_gc_list_mean,C_mean,yerr=C_std/sqrt(len(cat_ucd)),fmt='ko',label = 'All GCs')
-ax.errorbar(dis_ucd_gc_list_mean_b,C_mean_b,yerr=C_std_b/sqrt(len(cat_ucd)),fmt='bs',label = 'Blue GCs')
-ax.errorbar(dis_ucd_gc_list_mean_r,C_mean_r,yerr=C_std_r/sqrt(len(cat_ucd)),fmt='r^',label = 'Red GCs')
+ax.errorbar(dis_ucd_gc_list_mean,C_mean,yerr=C_std/sqrt(no_ucd),fmt='ko',label = 'All GCs')
+ax.errorbar(dis_ucd_gc_list_mean_b,C_mean_b,yerr=C_std_b/sqrt(no_ucd),fmt='bs',label = 'Blue GCs')
+ax.errorbar(dis_ucd_gc_list_mean_r,C_mean_r,yerr=C_std_r/sqrt(no_ucd),fmt='r^',label = 'Red GCs')
 ax.axhline(y=1,xmin=0,xmax=4,color='k')
 ax.set_xlabel('Aperture around UCD [kpc]',fontsize=16)
 ax.set_ylabel(r'C=<$\Sigma_{\rm measured}/\Sigma_{\rm expected}$>',fontsize=17)
@@ -233,7 +242,9 @@ ax.tick_params(which='minor',length=5)
 ax.set_xlim(0.25,4.5)
 ax.set_ylim(0,int(C_mean.max())+2)
 ax.legend(numpoints=1,frameon=False,loc='upper right')
-plt.savefig('clustering.new.blueandred.png')
-plt.savefig('./pics/clustering.blueandred'+str_radius[0]+'_'+str(step)+'.eps')
+plt.title('step: '+str(step)+', fitting max: '+str(fit_max)+', start: '+str(start)+'kpc')
+#plt.savefig('./pics/clustering_pics/clustering.bluered.'+str(start)+'.'+str(fit_max)+'.'+str(step)+'.png')
+plt.savefig('clustering.bluered.'+str(start)+'.'+str(fit_max)+'.'+str(step)+'.png')
+#plt.savefig('clustering.bluered.with.comp.1kpc'+str(start)+'.'+str(fit_max)+'.'+str(step)+'.png')
 
 logfile.close()
