60d7f2a36fc52d3653db20cca9314117456b258c
LejayChen <fiorano_chen@sjtu.edu.cn>
2017-03-16 12:02:30 +0800

    add mask for elliptical shape stat

--- gc_stat.py
+++ gc_stat.py
@@ -3,10 +3,18 @@ import numpy as np
 from math import *
 from astropy.table import Table
 
-np.set_printoptions(precision=2)
 M87 = (187.70583, 12.39111)
 DIS = 17.21*1000 # in kpc (luminosity distance from NED)
 radius_max = 500. #kpc   max radius for bin statistics (gc and ucd)
+
+def load_ucd_dEN_cat():
+	cat_ucd = Table.read('NGVS.pilot.92ucds.fits')
+	cat_dEN = Table.read('pp.gal.nuc2.s.master.new.fits')
+	ra_ucd_list = cat_ucd['RA']
+	dec_ucd_list = cat_ucd['DEC']
+	ra_dEN_list = cat_dEN['RA']
+	dec_dEN_list = cat_dEN['DEC']
+	return ra_ucd_list, dec_ucd_list, ra_dEN_list, dec_dEN_list
 
 def mask(bins_gc, gc_counts, dis_list, theta_list):
 
@@ -16,14 +24,12 @@ def mask(bins_gc, gc_counts, dis_list, theta_list):
 		theta_list_slice = theta_list_slice[dis_list_slice<float(bins_gc[i+1])]	
 		return theta_list_slice
 
-	cat_ucd = Table.read('NGVS.pilot.92ucds.fits')
-	cat_dEN = Table.read('pp.gal.nuc2.s.master.new.fits')
-
-	dis_ucd_list = np.sqrt((cat_ucd['RA']-M87[0])**2+(cat_ucd['DEC']-M87[1])**2) /180.*pi*DIS
-	dis_dEN_list = np.sqrt((cat_dEN['RA']-M87[0])**2+(cat_dEN['DEC']-M87[1])**2) /180.*pi*DIS
-
-	theta_ucd_list = np.arctan((cat_ucd['RA']-M87[0])/(cat_ucd['DEC']-M87[1]))
-	theta_dEN_list = np.arctan((cat_dEN['RA']-M87[0])/(cat_dEN['DEC']-M87[1]))
+	#load RA,DEC data from UCD and dE,N catalogs 
+	ra_ucd_list, dec_ucd_list, ra_dEN_list, dec_dEN_list = load_ucd_dEN_cat()
+	dis_ucd_list = np.sqrt((ra_ucd_list-M87[0])**2+(dec_ucd_list-M87[1])**2) /180.*pi*DIS
+	dis_dEN_list = np.sqrt((ra_dEN_list-M87[0])**2+(dec_dEN_list - M87[1])**2) /180.*pi*DIS
+	theta_ucd_list = np.arctan((ra_ucd_list - M87[0]) / (dec_ucd_list - M87[1]))
+	theta_dEN_list = np.arctan((ra_dEN_list - M87[0]) / (dec_dEN_list - M87[1]))	
 
 	gc_densities = []
 	areas = []  
@@ -42,7 +48,6 @@ def mask(bins_gc, gc_counts, dis_list, theta_list):
 		for theta in theta_dEN_list_slice:
 			theta_list_slice_bydEN = theta_list_slice[theta_list_slice<theta+0.05*pi]
 			theta_list_slice_bydEN = theta_list_slice_bydEN[theta_list_slice_bydEN>theta-0.05*pi]
-			gc_counts[i] = gc_counts[i] - len(theta_list_slice_bydEN)
 
 		area = (bins_gc[i+1]**2 - bins_gc[i]**2)*pi
 		area = area*(1- 0.1/2*(len(theta_list_slice_byucd)+len(theta_list_slice_bydEN)))
@@ -82,7 +87,7 @@ def gc_density_stat(cat_GC):
 	count_gc_binned = scipy.stats.binned_statistic(dis_list,dis_list,statistic='count',bins=bins_gc,range=(0.,radius_max))
 	bin_mean_gc = scipy.stats.binned_statistic(dis_list,dis_list,statistic='mean',bins=bins_gc,range=(0.,radius_max))[0] #mean value for distance in each bin
 
-	gc_counts = np.array(count_gc_binned[0],dtype='f8')
+	gc_counts = np.arragy(count_gc_binned[0],dtype='f8')
 	gc_counts_err = np.sqrt(gc_counts)
 		
 	gc_density, areas = mask(bins_gc, gc_counts, dis_list, theta_list)
@@ -90,39 +95,7 @@ def gc_density_stat(cat_GC):
 
 	return gc_density, gc_density_err,bin_mean_gc
 
-def elliptical_stat(cat, r_maj, PA=149./180*pi,e=0.5):
-	M87 = (187.70583, 12.39111)
-	DIS = 17.21*1000
-	r_min = e*r_maj
-	gc_counts = np.zeros(len(r_maj) -1) 
-
-	for i in range(len(cat)):   # for GC in GC catalog
-		ra_dis = (cat[i]['ra'] - M87[0])/180.*pi*DIS
-		dec_dis = (cat[i]['dec'] - M87[1])/180.*pi*DIS
-		dis = sqrt(ra_dis**2+dec_dis**2)
-		theta = np.arctan(dec_dis/ra_dis) - PA
-		for j in range(len(r_maj)):   # for ring in elliptical rings
-			if dis > r_maj[j]:
-				continue
-			else:
-				if dis>sqrt((r_maj[j]*cos(theta))**2+(r_min[j]*sin(theta))**2):
-					continue
-				else:
-					if j>=1: gc_counts[j-1] += 1
-					break
-
-	gc_counts_err = np.sqrt(gc_counts)
-	areas = np.array([],dtype='f4')
-	for i in range(len(r_maj[:-1])):
-		areas = np.append(areas, pi*e*(r_maj[i+1]**2-r_maj[i]**2))
-	gc_density = gc_counts/areas
-	gc_density_err = gc_counts_err/areas
-
-	return gc_density, gc_density_err
-
 def  which_ring(ra,dec,r_maj, PA=149./180*pi, e=0.5):
-	M87 = (187.70583, 12.39111)
-	DIS = 17.21*1000
 	r_min = e*r_maj
 	ra_dis = (ra - M87[0])/180.*pi*DIS
 	dec_dis = (dec - M87[1])/180.*pi*DIS
@@ -137,9 +110,73 @@ def  which_ring(ra,dec,r_maj, PA=149./180*pi, e=0.5):
 				continue
 			else:
 				return j-1
-	return len(r_maj)-1
+	return None
+
+def mask_e(r_maj, gc_counts, areas, gc_ra_list, gc_dec_list, mask_r=2):
+
+	mask_r  =2.0 #kpc
+	def count_gc_mask_r(ra,dec,gc_ra_list, gc_dec_list):
+		mask = np.sqrt((gc_ra_list-ra)**2 + (gc_dec_list)**2)/180.*pi*DIS<mask_r
+		gc_ra_list = gc_ra_list[mask]
+		return len(gc_ra_list)
+
+	ra_ucd_list, dec_ucd_list, ra_dEN_list, dec_dEN_list = load_ucd_dEN_cat()
+	for i in range(len(ra_ucd_list)):
+		ra = ra_ucd_list[i]
+		dec = dec_ucd_list[i]
+		index = which_ring(ra,dec,r_maj)
+		if index == None: continue
+
+		areas[index] = areas[index] - pi*mask_r**2
+		gc_counts[index] = gc_counts[index] - count_gc_mask_r(ra,dec,np.copy(gc_ra_list), np.copy(gc_dec_list))
+
+	return np.array(gc_counts),np.array(areas)
+
+def elliptical_stat(cat, r_maj, PA=149./180*pi,e=0.5):
+	r_min = e*r_maj
+	gc_counts = np.zeros(len(r_maj) -1) 
+	gc_ra_list = cat['ra']
+	gc_dec_list  =cat['dec']
+
+	for i in range(len(cat)):   # for GC in GC catalog
+		ra_dis = (gc_ra_list[i] - M87[0])/180.*pi*DIS
+		dec_dis = (gc_dec_list[i] - M87[1])/180.*pi*DIS
+		dis = sqrt(ra_dis**2+dec_dis**2)
+		theta = np.arctan(dec_dis/ra_dis) - PA
+		for j in range(len(r_maj)):   # for ring in elliptical rings
+			if dis > r_maj[j]:
+				continue
+			else:
+				if dis>sqrt((r_maj[j]*cos(theta))**2+(r_min[j]*sin(theta))**2):
+					continue
+				else:
+					if j>=1: gc_counts[j-1] += 1
+					break
+	
+	areas = np.array([],dtype='f4')
+	for i in range(len(r_maj[:-1])):
+		areas = np.append(areas, pi*e*(r_maj[i+1]**2-r_maj[i]**2))
+	gc_counts, areas = mask_e(r_maj, gc_counts, areas, gc_ra_list, gc_dec_list)
+	gc_density = gc_counts/areas
+
+	gc_counts_err = np.sqrt(gc_counts)
+	gc_density_err = gc_counts_err/areas
+
+	return gc_density, gc_density_err
+
+def  cal_r_maj(ra,dec, PA=149./180*pi, e=0.5):
+	M87 = (187.70583, 12.39111)
+	DIS = 17.21*1000
+	ra_dis = (ra - M87[0])/180.*pi*DIS
+	dec_dis = (dec - M87[1])/180.*pi*DIS
+
+	dis = sqrt(ra_dis**2+dec_dis**2)
+	theta = np.arctan(dec_dis/ra_dis) - PA
+
+	r_maj_ucd = sqrt(dis/(cos(theta**2)+(e*sin(theta))**2))
+	return r_maj_ucd
 
 def bkg_e(ra,dec,r_maj,intercept,slope):
-	exp_density = exp(intercept)*r_maj[which_ring(ra,dec,r_maj)]**(slope) 
+	exp_density = exp(intercept)*cal_r_maj(ra,dec,e=0.5)**(slope) 
 	bkg_unif = exp(intercept)*230**(slope)   	
 	return exp_density, bkg_unif
