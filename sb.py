12b61ee84bca00ace08bbc9843c382317056edb8
LejayChen <fiorano_chen@sjtu.edu.cn>
2017-04-11 14:54:36 +0800

    SEx command improvement

--- sb.py
+++ sb.py
@@ -9,10 +9,19 @@ cat_dEN = Table.read('../pp.gal.nuc2.s.master.new.fits')
 cat_ucd = Table.read('../NGVS.pilot.92ucds.fits')  
 
 def run_sex(FileName,cat_type):
+	assoc_name = 'position'
+	cat_name = './sex_fits/'+cat_type+'.'+FileName+'.fits '
+	check_img = './sex_fits/'+ cat_type+'.'+FileName+'.check.fits'
 	if cat_type == 'ucd':
-		os.system('sextractor  ../ucd_newcut.'+FileName+'.fits'+' -c ngvs.sex -CATALOG_NAME '+'./sex_fits/'+cat_type+'.'+FileName+'.fits '+'-CHECKIMAGE_NAME '+'./sex_fits/'+ cat_type+'.'+FileName+'.check.fits')
+		os.system('sextractor  ../ucd_newcut.'+FileName+'.fits'
+			+' -c ngvs.sex -CATALOG_NAME ' + cat_name
+			+' -ASSOC_NAME ' + assoc_name
+			+' -CHECKIMAGE_NAME '+check_img)
 	elif cat_type == 'dEN':
-		os.system('sextractor  ../dEN_pics/dE.N.'+FileName+'.fits'+' -c ngvs.sex -CATALOG_NAME '+'./sex_fits/'+cat_type+'.'+FileName+'.fits '+'-CHECKIMAGE_NAME '+'./sex_fits/'+ cat_type+'.'+FileName+'.check.fits')
+		os.system('sextractor  ../dEN_pics/dE.N.'+FileName+'.fits'
+			+' -c ngvs.sex -CATALOG_NAME ' + cat_name
+			+' -ASSOC_NAME ' + assoc_name
+			+' -CHECKIMAGE_NAME '+check_img)
 	else:
 		raise KeyError('cat_type argument only takes ucd or dEN')
 
@@ -22,23 +31,6 @@ def img_scale(img_name):
 	y_width = header['NAXIS2']
 	return x_width, y_width
 
-def find_central_obj(catalog, ra, dec,cat_type):
-	x,y = catalog['X_IMAGE'], catalog['Y_IMAGE']
-	if cat_type == 'ucd':
-		img_width_x, img_width_y = img_ucd_width_x, img_ucd_width_y
-	elif cat_type == 'dEN':
-		img_width_x, img_width_y = img_dEN_width_x, img_dEN_width_y
-
-	limit = 20
-	x_slice = np.copy(x)
-	while len(x_slice)>1:
-		x_slice = x[abs(x - img_width_x/2.)<limit]
-		y_slice = y[abs(x - img_width_x/2.)<limit]
-		x_slice = x_slice[abs(y_slice - img_width_y/2.)<limit]		
-		y_slice = y_slice[abs(y_slice - img_width_y/2.)<limit]
-		limit = limit/2. + 0.5
-	return np.where(x==x_slice[0])[0][0]
-
 def growth_curve(ID, cat_type, radii,mags):
 	final_sb = mags[-1]
 	r_edge = radii[-1]
@@ -47,8 +39,10 @@ def growth_curve(ID, cat_type, radii,mags):
 			r_edge = radii[i+1]
 			plt.plot(radii[i+1],mags[i+1],'^g')
 			break
-
-	r1,r2 = 0.35*r_edge, 0.7*r_edge
+	if cat_type == 'ucd':
+		r1,r2 = 0.6*r_edge, 0.9*r_edge
+	else:
+		r1,r2 = 0.33*r_edge, 0.6*r_edge
 	index1  = len(radii[radii<r1]) - 1
 	index2  = len(radii[radii<r2]) - 1	
 
@@ -64,9 +58,9 @@ def growth_curve(ID, cat_type, radii,mags):
 
 def cal_sb(ID, ra, dec, cat_type):
 	catalog = Table.read('sex_fits/'+cat_type+'.'+str(ID)+'.fits')
-	obj_id =  find_central_obj(catalog, ra, dec,cat_type)
+	obj_id =  0
 
-	radii = np.array([4, 8,12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96,110,130, 150])*0.187/2  # in arcsec
+	radii = np.array([2,4,6, 8,10,12,14, 16,18, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96,110,130, 150])*0.187/2  # in arcsec
 	mags = catalog[obj_id]['MAG_APER']
 	fluxes = 10**((mags - 30)/(-2.5))
 
@@ -90,6 +84,7 @@ def plot_hist(sbs,cat_type):
 	plt.xlabel(r'surface brightness ${\rm mag/arcsec^2}$')
 	plt.title(cat_type + ' surface brightness histogram')
 	plt.savefig('../pics/'+cat_type+'_sb_hist.png')
+	plt.clf()
 	print cat_type+' fig saved'
 
 img_ucd_width_x, img_ucd_width_y = img_scale('../ucd_newcut.31819.fits')
@@ -99,10 +94,11 @@ for cat in [(cat_ucd,'ucd'), (cat_dEN,'dEN')]:
 	for obj in cat[0]:
 		cat_type = cat[1]
 		ID = obj['INDEX']
-		#run_sex(str(ID), str(cat_type))
+		run_sex(str(ID), str(cat_type))
 		ra,dec = obj['RA'],obj['DEC']
 		sb = cal_sb(ID, ra, dec, cat_type)
 		sb_column.append(sb)
 		print cat_type, ID,sb
+
 	write_table(sb_column, cat_type)
 	plot_hist(np.array(sb_column),cat_type)
